7d71ade39dde6da32f32ede5ccbf5570171c75d2
Conanjun <1526840124@qq.com>
2017-12-03 12:21:07 +0800

    add templaate and 2

---
+++ template.py
@@ -0,0 +1,33 @@
+#  -*- coding: utf-8 -*-
+# Author: Conan0xff
+# Mail  : 1526840124@qq.com
+# Func  :
+
+
+'''
+	 Title 
+	
+	description
+
+'''
+
+class Solution(object):
+	pass
+
+
+
+import unittest
+
+
+class SolutionTest(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_solution(self):
+		pass
+
+if __name__ == "__main__":
+	unittest.main()
